487d1fd56e40789122ed73bfb85ee08a51dcfd23
faceq <59789496+nikrud131@users.noreply.github.com>
2022-01-11 19:08:43 +0300

    Create index.js

---
+++ lab2/src/index.js
@@ -0,0 +1,7 @@
+import menuIcon from './helpers/getMenuIcon';
+import bagIcon from './helpers/getBagIcon';
+import handleMenu from './helpers/handleMenu';
+
+
+menuIcon.onclick = handleMenu;
+bagIcon.onclick = handleMenu;
